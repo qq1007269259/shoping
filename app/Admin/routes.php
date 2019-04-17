a7322325c4e75726e3c6be2544be0e3a56f1f047
vagrant <vagrant@homestead>
2019-04-17 07:14:28 +0000

    管理后台-新增，编辑，展示商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,4 +16,12 @@ Route::group([
 
     $router->get('products', 'ProductsController@index');
 
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
+
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
+
+    $router->get('products/{id}', 'ProductsController@show');
+
 });
