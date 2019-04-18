3474b5b7f01faf65cc0c9cbfd4eec3cd7ee8a57b
vagrant <vagrant@homestead>
2019-04-18 07:04:24 +0000

    用户界面-商品列表

--- config/admin.php
+++ config/admin.php
@@ -3,49 +3,23 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | 站点标题
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of laravel-admin, This setting is displayed on the
-    | login page.
-    |
-    */
+     * 站点标题
+     */
     'name' => 'Laravel Shop',
 
     /*
-    |--------------------------------------------------------------------------
-    | 页面顶部 Logo
-    |--------------------------------------------------------------------------
-    |
-    | The logo of all admin pages. You can also set it as an image by using a
-    | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
-    |
-    */
+     * 页面顶部 Logo
+     */
     'logo' => '<b>Laravel</b> Shop',
 
     /*
-    |--------------------------------------------------------------------------
-    | 页面顶部小 Logo
-    |--------------------------------------------------------------------------
-    |
-    | The logo of all admin pages when the sidebar menu is collapsed. You can
-    | also set it as an image by using a `img` tag, eg
-    | '<img src="http://logo-url" alt="Admin logo">'.
-    |
-    */
-    'logo-mini' => '<b>La</b>',
+     * 页面顶部小 Logo
+     */
+    'logo-mini' => '<b>LS</b>',
 
     /*
-    |--------------------------------------------------------------------------
-    | 路由配置
-    |--------------------------------------------------------------------------
-    |
-    | The routing configuration of the admin page, including the path prefix,
-    | the controller namespace, and the default middleware. If you want to
-    | access through the root path, just set the prefix to empty string.
-    |
-    */
+     * 路由配置
+     */
     'route' => [
         // 路由前缀
         'prefix' => 'admin',
@@ -56,48 +30,23 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel-Admin 的安装目录
-    |--------------------------------------------------------------------------
-    |
-    | The installation directory of the controller and routing configuration
-    | files of the administration page. The default is `app/Admin`, which must
-    | be set before running `artisan admin::install` to take effect.
-    |
-    */
+     * Laravel-Admin 的安装目录
+     */
     'directory' => app_path('Admin'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel-Admin 页面标题
-    |--------------------------------------------------------------------------
-    |
-    | Html title for all pages.
-    |
-    */
+     * Laravel-Admin 页面标题
+     */
     'title' => 'Laravel Shop 管理后台',
 
     /*
-    |--------------------------------------------------------------------------
-    | 是否使用 https
-    |--------------------------------------------------------------------------
-    |
-    | If your page is going to be accessed via https, set it to `true`.
-    |
-    */
-    'https' => env('ADMIN_HTTPS', false),
+     * 是否使用 https
+     */
+    'secure' => env('ADMIN_HTTPS', false),
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel-Admin 用户认证设置
-    |--------------------------------------------------------------------------
-    |
-    | Authentication settings for all admin pages. Include an authentication
-    | guard and a user provider setting of authentication driver.
-    |
-    | You can specify a controller for `login` `logout` and other auth routes.
-    |
-    */
+     * Laravel-Admin 用户认证设置
+     */
     'auth' => [
 
         'controller' => App\Admin\Controllers\AuthController::class,
@@ -118,20 +67,12 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel-Admin 文件上传设置
-    |--------------------------------------------------------------------------
-    |
-    | File system configuration for form upload files and images, including
-    | disk and upload path.
-    |
-    */
+     * Laravel-Admin 文件上传设置
+     */
     'upload' => [
+        // 对应 filesystem.php 中的 disks
+        'disk' => 'public',
 
-        // Disk in `config/filesystem.php`.
-        'disk' => 'admin',
-
-        // Image and file upload path under the disk above.
         'directory' => [
             'image' => 'images',
             'file'  => 'files',
@@ -139,13 +80,8 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel-Admin 数据库设置
-    |--------------------------------------------------------------------------
-    |
-    | Here are database settings for laravel-admin builtin model & tables.
-    |
-    */
+     * Laravel-Admin 数据库设置
+     */
     'database' => [
 
         // 数据库连接名称，留空即可
@@ -176,27 +112,14 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel-Admin 操作日志设置
-    |--------------------------------------------------------------------------
-    |
-    | By setting this option to open or close operation log in laravel-admin.
-    |
-    */
+     * Laravel-Admin 操作日志设置
+     */
     'operation_log' => [
 
         'enable' => true,
 
         /*
-         * Only logging allowed methods in the list
-         */
-        'allowed_methods' => ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'],
-
-        /*
          * 不记操作日志的路由
-         *
-         * All method to path like: admin/auth/logs
-         * or specific method to path like: get:admin/auth/logs.
          */
         'except' => [
             'admin/auth/logs*',
@@ -204,113 +127,60 @@ return [
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | 地图组件提供商
-    |--------------------------------------------------------------------------
-    |
-    | Supported: "tencent", "google", "yandex".
-    |
-    */
+     * 地图组件提供商
+     */
     'map_provider' => 'google',
 
     /*
-    |--------------------------------------------------------------------------
-    | 页面风格
-    |--------------------------------------------------------------------------
-    |
-    | This value is the skin of admin pages.
-    | @see https://adminlte.io/docs/2.4/layout
-    |
-    | Supported:
-    |    "skin-blue", "skin-blue-light", "skin-yellow", "skin-yellow-light",
-    |    "skin-green", "skin-green-light", "skin-purple", "skin-purple-light",
-    |    "skin-red", "skin-red-light", "skin-black", "skin-black-light".
-    |
-    */
+     * 页面风格
+     * @see https://adminlte.io/docs/2.4/layout
+     */
     'skin' => 'skin-blue-light',
 
     /*
-    |--------------------------------------------------------------------------
-    | Application layout
-    |--------------------------------------------------------------------------
-    |
-    | This value is the layout of admin pages.
-    | @see https://adminlte.io/docs/2.4/layout
-    |
-    | Supported: "fixed", "layout-boxed", "layout-top-nav", "sidebar-collapse",
-    | "sidebar-mini".
-    |
-    */
+    |---------------------------------------------------------|
+    |LAYOUT OPTIONS | fixed                                   |
+    |               | layout-boxed                            |
+    |               | layout-top-nav                          |
+    |               | sidebar-collapse                        |
+    |               | sidebar-mini                            |
+    |---------------------------------------------------------|
+     */
     'layout' => ['sidebar-mini', 'sidebar-collapse'],
 
     /*
-    |--------------------------------------------------------------------------
-    | 登录页背景图
-    |--------------------------------------------------------------------------
-    |
-    | This value is used to set the background image of login page.
-    |
-    */
+     * 登录页背景图
+     */
     'login_background_image' => '',
 
     /*
-    |--------------------------------------------------------------------------
-    | 显示版本
-    |--------------------------------------------------------------------------
-    |
-    | Whether to display the version number of laravel-admim at the footer of
-    | each page
-    |
-    */
+     * 显示版本
+     */
     'show_version' => true,
 
     /*
-    |--------------------------------------------------------------------------
-    | 显示环境
-    |--------------------------------------------------------------------------
-    |
-    | Whether to display the environment at the footer of each page
-    |
-    */
+     * 显示环境
+     */
     'show_environment' => true,
 
     /*
-    |--------------------------------------------------------------------------
-    | 菜单绑定权限
-    |--------------------------------------------------------------------------
-    |
-    | whether enable menu bind to a permission
-    */
+     * 菜单绑定权限
+     */
     'menu_bind_permission' => true,
 
     /*
-    |--------------------------------------------------------------------------
-    | 默认启用面包屑
-    |--------------------------------------------------------------------------
-    |
-    | Whether enable default breadcrumb for every page content.
-    */
+     * 默认启用面包屑
+     */
     'enable_default_breadcrumb' => true,
 
     /*
-    |--------------------------------------------------------------------------
-    | 扩展所在的目录.
-    |--------------------------------------------------------------------------
-    |
-    | When you use command `php artisan admin:extend` to generate extensions,
-    | the extension files will be generated in this directory.
-    */
+     * 扩展所在的目录.
+     */
     'extension_dir' => app_path('Admin/Extensions'),
 
     /*
-    |--------------------------------------------------------------------------
-    | 扩展设置.
-    |--------------------------------------------------------------------------
-    |
-    | You can find all available extensions here
-    | https://github.com/laravel-admin-extensions.
-    |
-    */
+     * 扩展设置.
+     */
     'extensions' => [
 
     ],
