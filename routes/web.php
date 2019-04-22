d517c410209373d849c51a6adb9142062c61d0b9
vagrant <vagrant@homestead>
2019-04-22 08:37:00 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
-
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
 // 在之前的路由后面配上中间件
